480c00ea0c2d29a03082ed3b9d0c2de783698cfd
t-heu <teuzin375@gmail.com>
2025-03-31 20:32:43 -0300

    feat: add registers log

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { app, BrowserWindow, ipcMain, dialog } from 'electron';
 import path from 'path';
 import fs from 'fs';
+import os from"os";
 
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
@@ -11,6 +12,10 @@ if (require('electron-squirrel-startup')) {
 
 const downloadsPath = process.env.NODE_ENV === 'development' ? __dirname : path.join(process.env.USERPROFILE, 'Downloads');
 let isCancelled = false;
+
+process.on("uncaughtException", (error) => {
+  logError("Error: ", error);
+})
 
 const createWindow = (): void => {
   const mainWindow = new BrowserWindow({
@@ -67,7 +72,7 @@ const handleDownloadAllPDFs = async (mainWindow: BrowserWindow, event: any, tota
     event.reply("pdf-completed");
   } catch (error) {
     console.error('Erro ao gerar PDFs:', error);
-    notifyUser("Erro ao Gerar PDF", `Ocorreu um erro durante a geração do PDF. Detalhes: ${error.message}`);
+    logError("Erro ao Gerar PDF", error);
   }
 };
 
@@ -79,7 +84,7 @@ const handleDownloadSinglePDF = async (mainWindow: BrowserWindow) => {
     notifyUser("PDF Gerado com Sucesso", "PDF wms.pdf foi baixado na pasta de Downloads.");
   } catch (error) {
     console.error('Erro ao gerar PDF:', error);
-    notifyUser("Erro ao Gerar PDF", `Ocorreu um erro durante a geração do PDF. Detalhes: ${error.message}`);
+    logError("Erro ao Gerar PDF", error);
   }
 };
 
@@ -90,6 +95,48 @@ const notifyUser = (title: string, message: string) => {
     message,
     buttons: ["OK"],
   });
+};
+
+const logError = (title: string, error: Error) => {
+  dialog.showMessageBox({
+    type: "error",
+    title: "Erro ao Gerar PDF",
+    message: `Ocorreu um erro durante a geração do PDF. Detalhes: ${error.message}`,
+    buttons: ["OK"],
+  });
+
+  const logPath = path.join(app.getPath("userData"), "error_wmslabeler.log");
+  
+  // Capturar uso de CPU e memória no momento do erro
+  const cpuUsage = process.getCPUUsage(); // Uso de CPU pelo processo Electron
+  const memoryUsage = process.memoryUsage(); // Uso de memória detalhado
+  
+  // Capturar informações do sistema
+  const systemInfo = `
+    Sistema Operacional: ${os.platform()} - ${os.release()}
+    Arquitetura: ${os.arch()}
+    CPU: ${os.cpus()[0].model}
+    Memória Total: ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB
+    Memória Livre: ${(os.freemem() / 1024 / 1024).toFixed(2)} MB
+    Uso de CPU pelo Electron: ${(cpuUsage.percentCPUUsage * 100).toFixed(2)}%
+    Memória RSS (Total usada pelo processo): ${(memoryUsage.rss / 1024 / 1024).toFixed(2)} MB
+    Heap Total: ${(memoryUsage.heapTotal / 1024 / 1024).toFixed(2)} MB
+    Heap Usado: ${(memoryUsage.heapUsed / 1024 / 1024).toFixed(2)} MB
+  `;
+
+  // Criar conteúdo do log
+  const logContent = `
+    [${new Date().toISOString()}] - ${title}
+    Erro: ${error.message}
+    Stacktrace: ${error.stack}
+    
+    App Versão: ${app.getVersion()}
+    Caminho do App: ${app.getPath("exe")}
+    ${systemInfo}
+    ----------------------------
+  `;
+
+  fs.appendFileSync(logPath, logContent);
 };
 
 app.on('ready', createWindow);
